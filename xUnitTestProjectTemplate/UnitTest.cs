f69407b47071e9b30f420a91459664cc355c882f
Stephen Clarke <sclarke81@gmail.com>
2017-07-20 14:58:58 +0100

    Switch to a visual studio solution

---
+++ xUnitTestProjectTemplate/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+$if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+$endif$using System.Text;
+using Xunit;
+
+namespace $safeprojectname$
+{
+    public class UnitTest
+    {
+        [Fact]
+        public void TestMethod()
+        {
+        }
+    }
+}
